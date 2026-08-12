62dba92b77aa7543dc7a7878921ddd6a0e8eda04
prashantprayagii <hgg793957@gmail.com>
2026-08-12 22:14:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function updateTicketPrice() {
         priceDisplay.innerText = '₹299';
         if (performerFields) performerFields.style.display = 'block';
     } else {
-        priceDisplay.innerText = '₹99';
+        priceDisplay.innerText = '₹01';
         if (performerFields) performerFields.style.display = 'none';
     }
 }
